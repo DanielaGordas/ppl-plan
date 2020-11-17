826f0c3e5aaf90b95410350cd1b953746da4d66d
Daniela Gordas <daniela.gordas@gmail.com>
2020-11-17 15:25:07 +0100

    implemented function that saves the guest answers to the DB

--- client/src/games/low-carbon-travel/LowCarbonGame.js
+++ client/src/games/low-carbon-travel/LowCarbonGame.js
@@ -5,29 +5,50 @@ import axios from 'axios';
 import classes from '../../styles/components/box.module.scss';
 
 const LowCarbonGame = () => {
+    // retrieves answers from Local Storage
 
-    const [answers, setAnswers] = useState([]);
+    const guestAnswers = JSON.parse(window.localStorage.getItem('answers'));
+    const [answers, setAnswers] = useState(guestAnswers || []);
+    const guestDetails =JSON.parse(window.localStorage.getItem('guest'));
 
-    // const [topics, setTopics] = useState([]);
-
-    // const fetchData = async () => {
-    //     const resp = await axios.get('api/topics.json');
-    //     if(resp.data != null) {
-    //         setTopics(resp.data);
-    //     }
-    // };
-    
-    
-    // useEffect(() => {
-    //     fetchData();
-    // }, [])
-
+    // fetch all answers for game with id 1 from the API
     useEffect(() => {
         axios.get('/api/games/1/answers.json')
             .then(res => setAnswers(res.data))
+            .catch(error => console.log(error))
           }, []);
 
-          console.log(answers)
+    // stores guest answers in Local Storage
+    useEffect(() => {
+        window.localStorage.setItem('answers', JSON.stringify(answers));
+    }, [answers])
+
+    // saves selected answers to a new array
+    const guestAnswerArray = [];
+    answers.map(answer => {
+        if(answer.selected === true)
+            guestAnswerArray.push(answer)
+    })
+
+    // saves guest_answers to the DB     
+    const submitAnswers = () => {
+
+        const qs = require('qs');
+
+        if(guestAnswerArray !== []) {
+            guestAnswerArray.forEach(answer => {
+                axios.post('/api/guest_answers', qs.stringify(
+                    {
+                      guest_answer:{
+                        answer_id: answer.id,
+                        guest_id: guestDetails.id
+                    }
+                    }))
+                    .then(res => console.log(res.data))
+                    .catch(err => console.log(err))
+            })
+        }
+    }
 
     // function that handles the selected key of each object in the cards array 
     const updateSelected = (id, newSelected) => {
@@ -48,7 +69,7 @@ const LowCarbonGame = () => {
                     <Card key={answer.id} {...answer} updateSelected={updateSelected} />
                 ))}
             </div>
-            <button className="Btn">Complete!</button>
+            <button className="Btn" onClick={submitAnswers}>Complete!</button>
         </div>
     )
 }
