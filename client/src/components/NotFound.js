791781bc6dd9adb5910853a1f01eee3403c34960
Daniela Gordas <daniela.gordas@gmail.com>
2020-11-20 14:54:02 +0100

    Added fix for Rails routes

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/button.scss';
+
+const NotFound = () => {
+    return(
+        <div style={{textAlign: 'center',}}>
+            <h1>404: Not found</h1>
+            <Link to="/"><button clssName="Btn">Back to home</button></Link>
+        </div>
+    )
+}
+
+export default NotFound;
