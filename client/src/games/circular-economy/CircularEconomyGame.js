d0a56d1d022477b2fe4deb589b48913c2ae7913c
Daniela Gordas <daniela.gordas@gmail.com>
2020-12-09 11:28:30 +0100

    added different columns to drag and drop functionality

--- client/src/games/circular-economy/CircularEconomyGame.js
+++ client/src/games/circular-economy/CircularEconomyGame.js
@@ -20,10 +20,14 @@ const MovableItem = ({name, setItems}) => {
         item: { name, type:'Our first type'},
         end: (item, monitor) => {
             const dropResult = monitor.getDropResult();
-            if(dropResult && dropResult.name === 'Column 1'){
-                changeItemColumn(item, 'Column 1')
+            if(dropResult && dropResult.name === 'All'){
+                changeItemColumn(item, 'All')
+            } else if (dropResult && dropResult.name === 'Individuals'){
+                changeItemColumn(item, 'Individuals')
+            } else if (dropResult && dropResult.name === 'Community') {
+                changeItemColumn(item, 'Community')
             } else {
-                changeItemColumn(item, 'Column 2')
+                changeItemColumn(item, 'Council')
             }
         },
         collect: (monitor) => ({
@@ -64,84 +68,85 @@ const CircularEconomyGame = () => {
         instructions: "Tap the issues to find out more. Then drag them into the box which represents who you think should look after or own that issue, service or item."
     }
 
+
     const circularEconomyAnswers = [
         {
             id: 1, 
             name: "Skills exchange events",
             description: "These events can build stronger communities and improve social equality by providing individuals with equal access to knowledge and skills, which will in turn boost local economies.",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 2, 
             name: "Food shares",
             description: "£17 billion worth of food ends up in landfill each year in the UK - much of this could have been eaten by those in need. Food shares are set up to combat waste by sharing surplus or unwanted food with those in the community, helping to improve social and economic equality and building stronger communities.",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 3, 
             name: "Clothes swaps",
             description: "Around 30% of clothing in wardrobes in the UK has not been worn for at least a year, and an estimated £140 million worth of used clothing also goes to landfill in the UK every year. Clothes swaps offer people the chance to exchange clothes, which helps build stronger communities and reduce clothing going to landfill, also saving you money!",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 4, 
             name: "Furniture swaps",
             description: "Furniture swaps and communal repairs/repair workshops could greatly extend the life of furniture and provide people with more sustainable options. 22 million small items of furniture are thrown away every year in the UK.",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 5, 
             name: "Cleaning of parks/beaches/public spaces",
             description: "Could the local community manage this better and more effectively than the local council or a private company?",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 6, 
             name: "Household waste and recycling collection",
             description: "Would you be happy for a community group to manage waste collection rather than the council?",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 7, 
             name: "Locally grown food",
             description: "Community allotments are an excellent way for children and young people to learn about growing food. Individual/family allotments offer great exercise and a regular supply of fruit and vegetables. A mixture of the two can benefit communities with sharing of surplus produce.",
-            name: 'Column 1'
+            name: 'All'
         },
         {
             id: 8, 
             title: "Home and garden tools",
             description: "The average person spends almost £200 per year on tools and uses them infrequently. A shared approach could save huge amounts of resources and money.",
-            name: 'Column 1'
+            name: 'All'
         },
         {
             id: 9, 
             title: "Cars",
             description: "Car sharing schemes help relieve local traffic congestion, saving up to £1,000 per year (on fuel costs, parking and other vehicle running costs).",
-            name: 'Column 1'
+            name: 'All'
         },
         {
             id: 10, 
             name: "Bikes",
             description: "Bike sharing schemes enable users to access bikes 24/7 in urban areas, facilitating the use of cycling rather than other non-sustainable modes of transport.  A healthy, eco-friendly and fun way to travel!",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 11, 
             name: "Books and access to libraries",
             description: "Shared libraries/collections of books, as well as book swaps, can save the need to buy lots of books, especially textbooks that are often only used for a short while. Books can be borrowed, swapped or donated for communal use, saving paper, resources, and money.",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 12, 
             name: "Childrens’ or pets’ toys",
             description: "Every year in the UK, 8.5 million nearly new toys are thrown out as children and pets grow out of, or lose interest in, them. These can be redistributed to other children or pets as needed, reducing the amount in landfill.",
-            column: 'Column 1'
+            column: 'All'
         },
         {
             id: 13, 
             name: "Electronics",
             description: "The UK could save £370 million if all the old small electricals that are either thrown away or hoarded were recycled.",
-            column: 'Column 1'
+            column: 'All'
         },
         
 
@@ -158,20 +163,26 @@ const CircularEconomyGame = () => {
             <MovableItem key={item.id} name={item.name} setItems={setItems} />
         ))
     }
+
     
     return(
         <div className="Container">            
             <DndProvider backend={isMobile ? TouchBackend : HTML5Backend }>
-                <Column title='Column 1' className='Column FirstColumn'>
-                    {returnItemsForColumn('Column 1')}
+                <Column title='All' className='Column FirstColumn'>
+                    {returnItemsForColumn('All')}
                 </Column>
-                <Column title='Column 2' className='Column SecondColumn'>
-                    {returnItemsForColumn('Column 2')}
+                <Column title='Individuals' className='Column SecondColumn'>
+                    {returnItemsForColumn('Individuals')}
+                </Column>
+                <Column title='Community' className='Column SecondColumn'>
+                    {returnItemsForColumn('Community')}
+                </Column>
+                <Column title='Council' className='Column SecondColumn'>
+                    {returnItemsForColumn('Council')}
                 </Column>
             </DndProvider>
         </div>
     )
 }
-
 
 export default CircularEconomyGame;
