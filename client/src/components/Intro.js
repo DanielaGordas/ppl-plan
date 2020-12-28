a75cb4d86f71f99e2a41c701408dee1eb3aecc6e
Daniela Gordas <daniela.gordas@gmail.com>
2020-12-28 15:52:19 +0100

    added linear gradient and improved design consistency for low carbon game

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -1,21 +1,28 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from '../styles/components/intro-result.module.scss';
 import '../styles/components/button.scss';
+import '../styles/components/nav.scss';
+import { BiArrowBack, BiExit } from "react-icons/bi";
 
 const Intro = ({text, link, game, background}) => {
   return (
     <>
-      <div className={classes.GameNav}>
+      <div className="GameNav">
+        <div className={classes.NavButton}>
+          <BiArrowBack className={classes.MarginRight}/> 
           <Link to={game}>Back</Link>
+        </div>
+        <div className={classes.NavButton}>
           <Link to='/user'>Exit Game</Link>
+          <BiExit className={classes.MarginLeft}/>
+        </div>
       </div>
-      <div className={classes.Background} style={{backgroundImage: `url(${background})`}}>
+      <div className={classes.Background} style={{backgroundImage: `linear-gradient(rgba(169, 219, 232, 1),rgba(255, 255, 255, 0.6)), url(${background})`}}>
         <div className={classes.SpeechBubble}>
           <p>{text}</p>
         </div>
-        <Link to={link} style={{textAlign: 'center', marginTop: '7rem'}}><button className={classes.Btn}>Begin!</button></Link>
+        <Link to={link} style={{textAlign: 'center', marginTop: '7rem'}}><button className="Btn-border">Begin!</button></Link>
       </div>
     </>
   )
