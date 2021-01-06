db9ba3dff0e0831558f4b27d29406133dc26ed48
katmcglo <cathaumann@gmail.com>
2021-01-06 18:23:23 +0100

    added logo, need to make background match navbar etc

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,11 +4,16 @@ import '../styles/components/nav.scss';
 
 const NavBar = () => {
     return(
-        <Menu right width={ '100%' }>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/privacy">Privacy Policy</a>
-        </ Menu >
+        <div style={{ "width": "100%", "display": "flex", "alignItems": "center", "color": "#102773", "background": "linear-gradient(rgba(174, 220, 232, 1), rgba(235, 249, 253, 1))"}}>
+            <div className="NavBarLogo"></div>
+            <div style={{"border": "1px solid magenta"}}>
+                <Menu right width={'100%'} style={{ "border": "1px solid green" }}>
+                    <a id="home" className="menu-item" href="/">Home</a>
+                    <a id="about" className="menu-item" href="/about">About</a>
+                    <a id="contact" className="menu-item" href="/privacy">Privacy Policy</a>
+                </ Menu >
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/button.scss'
-import Megaphone from '../components/Megaphone';
 import classes from '../styles/pages/home.module.scss';
 import logo from '../images/logo-tagline.jpg'; 
 import NavBar from '../components/NavBar';
-import {GoGlobe} from "react-icons/go";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const HomePage = () => {
-    return(
-        <>
-            <NavBar />
-            <div className={classes.Background}>
-                <h3 className={classes.Title}>Help us save the planet!</h3>
-                <p className={classes.SubTitle}>Call to action here!</p>
-                {/* <GoGlobe className={classes.HomeLogo}/> */}
-                <div className={classes.HomeLogo}></div>
-                <Link to="/user"><button className="Btn">Participate</button></Link>
-            </div>
-        </>
+return(
+<>
+  <NavBar />
+  <div className={classes.Background}>
+    <h3 className={classes.Title}>Help us save the planet!</h3>
+    <p className={classes.SubTitle}>Call to action here!</p>
+  <div className={classes.HomeLogo}></div>
+    <Link to="/user"><button className="Btn">Participate</button></Link>
+  </div>
+</>
     )
 }
 
