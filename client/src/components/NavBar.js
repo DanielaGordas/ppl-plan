6eb6ce04327470c31702cf2ee001ca884fd28da8
katmcglo <cathaumann@gmail.com>
2021-01-09 14:27:14 +0100

    added logo and main picture on index page. need to fix navbar color to work with linear gradient

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,10 +4,10 @@ import '../styles/components/nav.scss';
 
 const NavBar = () => {
     return(
-        <div style={{ "width": "100%", "display": "flex", "alignItems": "center", "color": "#102773", "background": "linear-gradient(rgba(174, 220, 232, 1), rgba(235, 249, 253, 1))"}}>
+        <div style={{ "width": "100%", "display": "flex", "alignItems": "center", "position": "relative"}}>
             <div className="NavBarLogo"></div>
-            <div style={{"border": "1px solid magenta"}}>
-                <Menu right width={'100%'} style={{ "border": "1px solid green" }}>
+            <div>
+                <Menu right width={'100%'}>
                     <a id="home" className="menu-item" href="/">Home</a>
                     <a id="about" className="menu-item" href="/about">About</a>
                     <a id="contact" className="menu-item" href="/privacy">Privacy Policy</a>
