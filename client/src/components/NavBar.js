06eb8b5508b51d88ad5f1d5b8791e9369af525e6
katmcglo <cathaumann@gmail.com>
2021-01-10 14:32:01 +0100

    removed logo for now

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,16 +4,16 @@ import '../styles/components/nav.scss';
 
 const NavBar = () => {
     return(
-        <div style={{ "width": "100%", "display": "flex", "alignItems": "center", "position": "relative"}}>
-            <div className="NavBarLogo"></div>
-            <div>
+        // <div style={{ "width": "100%", "display": "flex", "alignItems": "center", "position": "relative"}}>
+        //     <div className="NavBarLogo"></div>
+        //     <div>
                 <Menu right width={'100%'}>
                     <a id="home" className="menu-item" href="/">Home</a>
                     <a id="about" className="menu-item" href="/about">About</a>
                     <a id="contact" className="menu-item" href="/privacy">Privacy Policy</a>
                 </ Menu >
-            </div>
-        </div>
+        //     </div>
+        // </div>
     )
 }
 
