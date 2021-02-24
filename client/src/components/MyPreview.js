8556b7ed611b3904236c1f287fa572dd426dddbe
Daniela Gordas <daniela.gordas@gmail.com>
2021-02-24 16:52:56 +0100

    moved game and answers into a separate file and refactored the result component

--- client/src/components/MyPreview.js
+++ client/src/components/MyPreview.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { usePreview } from 'react-dnd-preview';
 
-const MyPreview = ({classes, icon}) => {
-  const {display, itemType, item, style} = usePreview();
+const MyPreview = ({classes}) => {
+  const {display, item, style} = usePreview();
   if (!display) {
     return null;
   }
   return (
-    <div class={classes.Card} style={style}>
+    <div className={classes.Card} style={style}>
       {item.icon ? <img src={item.icon} alt=""/> : item.name}
     </div>
   )
--- client/src/games/research-development/ResearchDevelopmentGame.js
+++ client/src/games/research-development/ResearchDevelopmentGame.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Intro from '../../components/Intro';
 import { Switch, Route, Link } from 'react-router-dom';
-import ResearchDevelopmentResult from './ResearchDevelopmentResult';
 import classes from '../../styles/pages/research-development.module.scss';
 import axios from 'axios';
 import { SwipeableList, SwipeableListItem } from '@sandstreamdev/react-swipeable-list';
 import '@sandstreamdev/react-swipeable-list/dist/styles.css';
 import { CollapsibleComponent, CollapsibleHead, CollapsibleContent} from "react-collapsible-component";
+import {researchDevelopmentGame, researchDevelopmentAnswers} from './Data';
+import Result from '../../components/Result';
 
 // Images and icons
 
@@ -17,92 +18,8 @@ import Background from '../../images/research-development/Game_8.svg';
 
 const ResearchDevelopmentGame = () => {
 
-    const researchDevelopmentGame = {
-        id: 8, 
-        title: "Research and Development",
-        intro: "The most famous climate lab in the world has been given some funding for an amazing new R&D project. All the scientists in the lab are competing for this money. The R&D projects are going head to head, and you decide which project wins!",
-        instructions: "Play each battle and decide the winner by swiping left. You will play 2 rounds leading to the grand final and an overall winner."
-    }
-
-    const researchDevelopmentAnswers = [
-        {
-            id: 1, 
-            name: "New Nuclear Power",
-            description: "Create a new generation of reactors, with improved safety and lower costs. They are called Small Modular Reactors (SMRs). They use advanced tech to be smaller and more flexible. They could help create zero carbon energy, and even help create synthetic aviation fuels. If done right, this industry could create tens of thousands of new jobs as part of the energy transition.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 2, 
-            name: "Win With Waste",
-            description: "About 25% of the UKs waste was disposed of at landfill in 2016 which still carries huge emissions both in CO2 and pollution. We could create better ways to reuse and transform waste into energy. Research in this area could create lots of green jobs and create all kinds of new biofuels and processes.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 3, 
-            name: "Excellent Energy Storage",
-            description: "Many renewable energies generate an inconsistent flow of electricity, sometimes producing none at all and other times producing an excess which gets wasted. Doing more research into the various chemical and other types of batteries will improve energy storage, reduce reliance on fossil fuels, save consumers money and make the electricity system more reliable and resilient",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 4, 
-            name: "Super Sustainable Agriculture",
-            description: "Agriculture uses 70% of UK land, so research into regenerative agricultural methods which treat farms as individual ecosystems that can be redesigned to work in sync with nature is crucial to improving soil health and sucking down carbon. Further benefits range from preventing flooding to strengthening the food supply chain.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 5, 
-            name: "Ocean Energy, Making Waves",
-            description: "Ocean energy generates electricity from waves, tides, currents, temperature differences or even salinity (salt) concentration differences in the ocean. With long coastlines and large tidal ranges, the UK is a good candidate for ocean technology. Wave and tidal stream energy has potential to meet up to 20% of our electricity demand and there are already a number of ongoing small scale projects using these technologies.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 6, 
-            name: "Critical Carbon Capture (Use & Storage)",
-            description: "Capturing carbon dioxide, either from industrial waste gases or direct from the air, could be groundbreaking for combatting emissions. Carbon capture is a particularly useful technology to help lower emissions from ‘hard to reach’ areas, such as heavy industry, or in developing countries whose economies may rely on fossil fuels to power development for decades to come.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 7, 
-            name: "Create Smart Cities",
-            description: "Cities are responsible for 75% of the world’s energy demand, research into smart tech could accelerate the shift toward a cleaner and less polluted urban space. Smart tech includes AI and data gathered from sensors, cameras, solar panels, apps, smart-meters, and even bins. The potential benefits are enormous.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-        {
-            id: 8, 
-            name: "Better Biofuel",
-            description: "Biofuels, although not the cleanest option, have potential to replace fossil fuels until longer term solutions are put in place. It does not require expensive adjustments or infrastructure to aid the transition and has already made great leaps in development. For example, through research we have already developed advanced biofuels, and can now produce biohydrogen which fuels vehicles with zero emissions.",
-            column: 'All',
-            game: "Research and Development",
-            svg: "",
-            selected: false
-        },
-    ]
-
-    const [game, setGame] = useState(researchDevelopmentGame);
-    const [items, setItems] = useState(researchDevelopmentAnswers);
-
-    const [info, setInfo] = useState([researchDevelopmentAnswers[0].name, researchDevelopmentAnswers[0].description])
+    const game = researchDevelopmentGame;
+    const items = researchDevelopmentAnswers;
 
     // retrieves guest user details from localStorage
     const guestDetails =JSON.parse(window.localStorage.getItem('guest'));
@@ -117,7 +34,7 @@ const ResearchDevelopmentGame = () => {
         window.localStorage.setItem('result8', JSON.stringify(researchDevelopmentResult));
     })
  
-     const resultText = "Research and development is a fantastic way to create new jobs and solve big problems! For helping pick the winning project you earn the Lab Badge!"
+     const resultText = "Research and development is a fantastic way to create new jobs and solve big problems!"
 
 
 
@@ -665,7 +582,7 @@ const ResearchDevelopmentGame = () => {
                         <Link to='/research-development/intro'>Back</Link>
                     </div>
                     <div className="NavLink">
-                        <a className="" onClick={startOver} >Start over</a>
+                        <a onClick={startOver} >Start over</a>
                         < BiRevision className="RightIcon"/>
                     </div>
                 </div>
@@ -677,7 +594,14 @@ const ResearchDevelopmentGame = () => {
                 </div>
             </Route>
             <Route path="/research-development/result">
-                <ResearchDevelopmentResult gradient={gradient} background={Background}/>
+                <Result 
+                    gradient={gradient} 
+                    background={Background} 
+                    text={researchDevelopmentText} 
+                    badge="You've earned the LAB BADGE"
+                    back="/research-development/game"
+                    next="/circular-economy/intro"
+                />
             </Route>
         </Switch>
     )
