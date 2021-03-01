eb1fac90be6ed45101ae74823b08965b53e070e9
Phil <phil.ayres@consected.com>
2021-03-01 09:27:53 +0000

    Update deployment instructions and script

--- README.md
+++ README.md
@@ -42,3 +42,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
+## Deployment to Elastic Beanstalk
+
+See [deploy/how_to_deploy.md](deploy/how_to_deploy.md)
--- deploy/aws-eb.sh
+++ deploy/aws-eb.sh
@@ -16,38 +16,70 @@ SERVERSIZE=t2.micro
 
 SETUP_DEV=${SETUP_DEV:=false}
 
-if [ "${SETUP_DEV}" == 'true' ]; then
-  # These are development settings only. Do not use in production
-  echo "Deploying to development"
-  EB_ENV=ppl-plan
-  SECRET_KEY_BASE=31293bb4adaf2e3a046c030554799a57efee9f0d08943b6b26e393d718fb64fdab86c4677d1627c29c0eff871672e198575d697d1d615821021509a8e327e3f3
-  RDS_DB_NAME=ppl_plan
-  RDS_USERNAME=ebdb
-  RDS_PASSWORD=test-ebrails-password
-  RDS_HOSTNAME=
-  ENV_FLAGS="-p ${EB_PLATFORM}"
-  DEV_FLAGS="-s -db -db.engine postgres -db.user ${RDS_USERNAME} -db.pass ${RDS_PASSWORD}"
-else
-  echo "Deploying to production. Provide gpg key to unlock production secrets."
-  gpg deploy/production-vars.sh.gpg
+function setup_env_vars() {
+  if [ "${SETUP_DEV}" == 'true' ]; then
+    # These are development settings only. Do not use in production
+    echo "Deploying to development"
+    EB_ENV=ppl-plan
+    SECRET_KEY_BASE=31293bb4adaf2e3a046c030554799a57efee9f0d08943b6b26e393d718fb64fdab86c4677d1627c29c0eff871672e198575d697d1d615821021509a8e327e3f3
+    RDS_DB_NAME=ppl_plan
+    RDS_USERNAME=ebdb
+    RDS_PASSWORD=test-ebrails-password
+    RDS_HOSTNAME=
+    ENV_FLAGS="-p ${EB_PLATFORM}"
+    DEV_FLAGS="-s -db -db.engine postgres -db.user ${RDS_USERNAME} -db.pass ${RDS_PASSWORD}"
+  else
+    echo "Deploying to production. Provide gpg key to unlock production secrets."
+    gpg deploy/production-vars.sh.gpg
 
-  if [ ! -f deploy/production-vars.sh ]; then
-    echo 'production-vars.sh was not decrypted. Incorrect gpg key used?'
-    exit 5
+    if [ ! -f deploy/production-vars.sh ]; then
+      echo 'production-vars.sh was not decrypted. Incorrect gpg key used?'
+      exit 5
+    fi
+
+    source deploy/production-vars.sh
+    rm -f deploy/production-vars.sh
+
+    EB_ENV=ppl-plan-prod
+    ENV_FLAGS="-p ${EB_PLATFORM} --scale 2 -pr --vpc.publicip"
+    ENV_FLAGS="${ENV_FLAGS} --vpc.id vpc-0ae947ca3485d3fb0 --vpc.securitygroups sg-0a38b3e8689f621ef"
+    ENV_FLAGS="${ENV_FLAGS} --vpc.ec2subnets subnet-0259f8e1ab3f8482d,subnet-0e28cb8715f47a3ed"
+    ENV_FLAGS="${ENV_FLAGS} --vpc.dbsubnets subnet-0ca858a5a97e7b9f4,subnet-06ce1cab0ec6db9d6,subnet-0e28cb8715f47a3ed,subnet-0259f8e1ab3f8482d"
+    ENV_FLAGS="${ENV_FLAGS} --vpc.elbpublic --vpc.elbsubnets subnet-0f6f49ad3067f7d95,subnet-0f9212ad3ec233590"
+  fi
+}
+
+function check_aws_credentials() {
+
+  got_profile=$(grep 'profile ppl-plan' ~/.aws/config)
+  if [ ! "${got_profile}" ]; then
+    echo "The AWS CLI config and credentials were not set up for ${EB_PROFILE}. The config has now been created."
+    echo "Configure credentials in ~/.aws/credentials for '${EB_PROFILE}' before continuing"
+
+    echo "Edit ~/.aws/credentials then re run this deployment."
+    cat >> ~/.aws/config << EOF
+
+[profile ppl-plan]
+region = eu-west-2
+output = json
+
+EOF
+
+    cat >> ~/.aws/credentials << EOF
+
+[ppl-plan]
+aws_access_key_id =  ???
+aws_secret_access_key = ???
+
+EOF
+
+    exit 2
   fi
 
-  source deploy/production-vars.sh
-  rm -f deploy/production-vars.sh
-
-  EB_ENV=ppl-plan-prod
-  ENV_FLAGS="-p ${EB_PLATFORM} --scale 2 -pr --vpc.publicip"
-  ENV_FLAGS="${ENV_FLAGS} --vpc.id vpc-0ae947ca3485d3fb0 --vpc.securitygroups sg-0a38b3e8689f621ef"
-  ENV_FLAGS="${ENV_FLAGS} --vpc.ec2subnets subnet-0259f8e1ab3f8482d,subnet-0e28cb8715f47a3ed"
-  ENV_FLAGS="${ENV_FLAGS} --vpc.dbsubnets subnet-0ca858a5a97e7b9f4,subnet-06ce1cab0ec6db9d6,subnet-0e28cb8715f47a3ed,subnet-0259f8e1ab3f8482d"
-  ENV_FLAGS="${ENV_FLAGS} --vpc.elbpublic --vpc.elbsubnets subnet-0f6f49ad3067f7d95,subnet-0f9212ad3ec233590"
-fi
+}
 
 echo "Yarn build and deploy"
+git fetch origin release
 git checkout release
 
 if [ "$?" != 0 ]; then
@@ -55,7 +87,7 @@ if [ "$?" != 0 ]; then
   exit 7
 fi
 
-git merge master
+git merge -s ours master
 
 if [ "$?" != 0 ]; then
   echo "'git merge master' failed "
@@ -71,36 +103,16 @@ fi
 
 git add -f public
 git commit -a -m "Build yarn for deployment"
+git push -f
 
-got_profile=$(grep 'profile ppl-plan' ~/.aws/config)
-if [ ! "${got_profile}" ]; then
-  echo "The AWS CLI config and credentials were not set up for ${EB_PROFILE}. The config has now been created."
-  echo "Configure credentials in ~/.aws/credentials for '${EB_PROFILE}' before continuing"
-
-  echo "Edit ~/.aws/credentials then re run this deployment."
-  cat >> ~/.aws/config << EOF
-
-[profile ppl-plan]
-region = eu-west-2
-output = json
-
-EOF
-
-  cat >> ~/.aws/credentials << EOF
-
-[ppl-plan]
-aws_access_key_id =  ???
-aws_secret_access_key = ???
-
-EOF
-
-  exit 2
-fi
+check_aws_credentials
 
 eb status ${EB_ENV} --profile ${EB_PROFILE}
 
 if [ "$?" == 4 ]; then
   echo "The EB environment has not been set up. Starting the configuration"
+
+  setup_env_vars
 
   eb create ${EB_ENV} \
     --profile ${EB_PROFILE} ${DEV_FLAGS} ${ENV_FLAGS} \
@@ -112,3 +124,5 @@ else
 fi
 
 git checkout master
+
+echo "Deployed. You are back on the master branch"
