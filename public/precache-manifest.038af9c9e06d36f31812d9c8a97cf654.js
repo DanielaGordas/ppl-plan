b7e161b566e4303a1d74aebf63bc03224b696247
Daniela Gordas <daniela.gordas@gmail.com>
2021-03-02 11:55:36 +0100

    Build yarn for deployment

---
+++ public/precache-manifest.038af9c9e06d36f31812d9c8a97cf654.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd8e592542dcb869acc7b5b5980cba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e008ee48960a7127ae",
+    "url": "/static/css/2.407546cd.chunk.css"
+  },
+  {
+    "revision": "02f76f524f1d158296ca",
+    "url": "/static/css/main.aaaafa4f.chunk.css"
+  },
+  {
+    "revision": "d9e008ee48960a7127ae",
+    "url": "/static/js/2.20d2eef5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.20d2eef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f76f524f1d158296ca",
+    "url": "/static/js/main.73dad841.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "33f61ba66ffb4b6dec0a6cb0c9879a5a",
+    "url": "/static/media/Bikes_blue_icon.33f61ba6.svg"
+  },
+  {
+    "revision": "1c7f4f00799d8e50637eee702de991c3",
+    "url": "/static/media/Car-free_zones.1c7f4f00.svg"
+  },
+  {
+    "revision": "db1cc3f3c07d6af1ba9747442d88779c",
+    "url": "/static/media/Car_scrappage_scheme.db1cc3f3.svg"
+  },
+  {
+    "revision": "3a15d848acc39a78f7a2792adc624bc3",
+    "url": "/static/media/Cars_blue_icon.3a15d848.svg"
+  },
+  {
+    "revision": "fd936f6bf1e27e028cabf5b30d125a62",
+    "url": "/static/media/Character 3a.fd936f6b.svg"
+  },
+  {
+    "revision": "0f835ee6a2253785a92de529471d6bf1",
+    "url": "/static/media/Character 6.0f835ee6.svg"
+  },
+  {
+    "revision": "a80140f23a3f3e0bb3b4871019bcb653",
+    "url": "/static/media/Character_1_First_screen.a80140f2.svg"
+  },
+  {
+    "revision": "849fe27c96772aef4cafcfbde1201b58",
+    "url": "/static/media/Character_7.849fe27c.svg"
+  },
+  {
+    "revision": "a7f4c3a8bbf94ccfcce27fb4eb854080",
+    "url": "/static/media/Character_Game_4.a7f4c3a8.svg"
+  },
+  {
+    "revision": "d92fc8cce32c2dfc6606cacbb366d755",
+    "url": "/static/media/Childrens_pets_toys_blue_icon.d92fc8cc.svg"
+  },
+  {
+    "revision": "56a09ecd245e9b9a6b42e259a8c38411",
+    "url": "/static/media/Clothes_swaps_blue_icon.56a09ecd.svg"
+  },
+  {
+    "revision": "1019405f652403f1033c2a49a16bfa34",
+    "url": "/static/media/Draught_proofing_blue_icon.1019405f.svg"
+  },
+  {
+    "revision": "b7906fa75d22fe3fbeef2947536468c3",
+    "url": "/static/media/EV_charging_points.b7906fa7.svg"
+  },
+  {
+    "revision": "dc97c60ac46d4c5d5c0e4b50d4db6328",
+    "url": "/static/media/Electric_bus_network.dc97c60a.svg"
+  },
+  {
+    "revision": "a4bf5391c40f1abbc7a4dc49bdd76471",
+    "url": "/static/media/Electric_rail_network.a4bf5391.svg"
+  },
+  {
+    "revision": "162660aa84b0825f9777036d430fcde2",
+    "url": "/static/media/Electronics_blue_icon.162660aa.svg"
+  },
+  {
+    "revision": "4af67acf32c8c34dd01777b62b5995a2",
+    "url": "/static/media/Employer_car_sharing_scheme.4af67acf.svg"
+  },
+  {
+    "revision": "fdc34328f93cb29607001637ba997ad2",
+    "url": "/static/media/External_internal_wall_insulation_blue_icon.fdc34328.svg"
+  },
+  {
+    "revision": "00b457066b8080ab421da690a517b232",
+    "url": "/static/media/Food_shares_blue_icon.00b45706.svg"
+  },
+  {
+    "revision": "b6795b698bc268c911193eb02d91e3e9",
+    "url": "/static/media/Furniture_swaps_blue_icon.b6795b69.svg"
+  },
+  {
+    "revision": "142e8e340ef9ab3a0841805d9addc222",
+    "url": "/static/media/Game_1_Badge_Colour.142e8e34.svg"
+  },
+  {
+    "revision": "b0a2ce664149b7bf520a2566e988883a",
+    "url": "/static/media/Game_1_Badge_Grey.b0a2ce66.svg"
+  },
+  {
+    "revision": "929b62e003ae40a889ef99a464db8eb8",
+    "url": "/static/media/Game_1_screen_1.929b62e0.svg"
+  },
+  {
+    "revision": "7315a6822052cbde6d5a5a82113229be",
+    "url": "/static/media/Game_2_Badge_Colour.7315a682.svg"
+  },
+  {
+    "revision": "13bb7283de86986fd36a2063f52f5a71",
+    "url": "/static/media/Game_2_Badge_Grey.13bb7283.svg"
+  },
+  {
+    "revision": "9f1c3b664e71b4a47cfbc413dbffb631",
+    "url": "/static/media/Game_3_Badge_Colour.9f1c3b66.svg"
+  },
+  {
+    "revision": "9338616e570fb3ce7cfa71c3ee5b6cb9",
+    "url": "/static/media/Game_3_Badge_Grey.9338616e.svg"
+  },
+  {
+    "revision": "aa2c476f8076d8db0dad92ea88b97b42",
+    "url": "/static/media/Game_3_new_screen.aa2c476f.svg"
+  },
+  {
+    "revision": "04ef792a3c9ae74858d978386ff5ef1c",
+    "url": "/static/media/Game_4_Badge_Colour.04ef792a.svg"
+  },
+  {
+    "revision": "3c94e3daf132c13420d2ec761f02f77e",
+    "url": "/static/media/Game_4_Badge_Grey.3c94e3da.svg"
+  },
+  {
+    "revision": "957bcc7c00f2bf83f355615d6daaa6e8",
+    "url": "/static/media/Game_4_First_Screen.957bcc7c.svg"
+  },
+  {
+    "revision": "4253513e1bb8ba73c3d07106cc19f5d5",
+    "url": "/static/media/Game_6_Badge_Colour.4253513e.svg"
+  },
+  {
+    "revision": "789a271bcb586fcb8262ea8bb7222603",
+    "url": "/static/media/Game_6_Badge_Grey.789a271b.svg"
+  },
+  {
+    "revision": "bcfca17faf2acc9168a2d0f9e53a5be0",
+    "url": "/static/media/Game_6_Community_gardens_icon.bcfca17f.svg"
+  },
+  {
+    "revision": "a2e9c5fe9ce76d7f76c3bbb04c12ee1f",
+    "url": "/static/media/Game_6_First_Screen.a2e9c5fe.svg"
+  },
+  {
+    "revision": "b2a31c3a3c195d19ba61d6834d8af5fe",
+    "url": "/static/media/Game_6_Green_bridges_icon.b2a31c3a.svg"
+  },
+  {
+    "revision": "67ea8e1b6f93dadc122a9511c44eed0d",
+    "url": "/static/media/Game_6_Green_roofs_walls_icon.67ea8e1b.svg"
+  },
+  {
+    "revision": "67e0fdbd4397a60664d6a0d0f424fe63",
+    "url": "/static/media/Game_6_Ponds_icon.67e0fdbd.svg"
+  },
+  {
+    "revision": "a7d1a7e8d51f9d30a3507ea1a0d7d258",
+    "url": "/static/media/Game_6_Public_parks_icon.a7d1a7e8.svg"
+  },
+  {
+    "revision": "f27e0aeebda348d078de7aa184bfe190",
+    "url": "/static/media/Game_6_Rewilded_areas_town_icon.f27e0aee.svg"
+  },
+  {
+    "revision": "b8f0cd463750124148a65fdba4e63333",
+    "url": "/static/media/Game_6_Urban_rewilding_schemes_icon.b8f0cd46.svg"
+  },
+  {
+    "revision": "79898cfbe35c4b3ec741d1b953dd841d",
+    "url": "/static/media/Game_6_Wild_road_verges_icon.79898cfb.svg"
+  },
+  {
+    "revision": "c9f79ca2287586bfae0715300cf0be72",
+    "url": "/static/media/Game_6_Woodland_areas_icon.c9f79ca2.svg"
+  },
+  {
+    "revision": "d0ad66ba84f59915167fa20a66e4a089",
+    "url": "/static/media/Game_7_Badge_Colour.d0ad66ba.svg"
+  },
+  {
+    "revision": "1d6ca196ac6ff2ab1119f8734806d144",
+    "url": "/static/media/Game_7_Badge_Grey.1d6ca196.svg"
+  },
+  {
+    "revision": "83bf0f8446de8f202bcb551c9891f5d3",
+    "url": "/static/media/Game_7_First_Screen.83bf0f84.svg"
+  },
+  {
+    "revision": "bba9f89c72632c8b2780bc0ac0243a16",
+    "url": "/static/media/Game_8_Badge_Colour.bba9f89c.svg"
+  },
+  {
+    "revision": "b08321085d0e2fc5164c457ab5caad51",
+    "url": "/static/media/Game_8_Badge_Grey.b0832108.svg"
+  },
+  {
+    "revision": "6763c7fbaec932e7c21264949073d016",
+    "url": "/static/media/Home_and_garden_tools_blue_icon.6763c7fb.svg"
+  },
+  {
+    "revision": "44fe32bd52b101090d1af33c0afbc337",
+    "url": "/static/media/Hydrogen_boilers_Blue_icon.44fe32bd.svg"
+  },
+  {
+    "revision": "74243fd7e85d38b4003356b2ba31ced8",
+    "url": "/static/media/Improve_existing_public_transport.74243fd7.svg"
+  },
+  {
+    "revision": "64d3658111f55531565e970cde38ae87",
+    "url": "/static/media/Locally_grown_food_blue_icon.64d36581.svg"
+  },
+  {
+    "revision": "96a6d5001e7e9aa86848054cb493a1aa",
+    "url": "/static/media/National_cycling_network.96a6d500.svg"
+  },
+  {
+    "revision": "813ed122cc8cfe1f9d831c3e28558cd7",
+    "url": "/static/media/Purchase_grants.813ed122.svg"
+  },
+  {
+    "revision": "852151cdbc79e1be4caac37b92307fc4",
+    "url": "/static/media/Rainwater_harvesting_system_blue_icon.852151cd.svg"
+  },
+  {
+    "revision": "7e70ecfe9924fe8e8e03e16625524c8b",
+    "url": "/static/media/Smart_controls_blue_icon.7e70ecfe.svg"
+  },
+  {
+    "revision": "6d6c4390632f1da5d3150aa3f488311b",
+    "url": "/static/media/Smart_hybrid_heat_pumps_blue_icon.6d6c4390.svg"
+  },
+  {
+    "revision": "7f1ed4c7e69e15c2f488eac9e3c7218f",
+    "url": "/static/media/Solar_panels_blue_icon.7f1ed4c7.svg"
+  },
+  {
+    "revision": "1a91418ca7f51627f0b6a4e449bc579f",
+    "url": "/static/media/Tax_benefits.1a91418c.svg"
+  },
+  {
+    "revision": "28761076d926c204aba87b8046e18819",
+    "url": "/static/media/Triple_glazed_windows_blue_icon.28761076.svg"
+  },
+  {
+    "revision": "40e01f942e2ac85c16269335aa5d01ea",
+    "url": "/static/media/Whole_House_Energiesprong_blue_icon.40e01f94.svg"
+  },
+  {
+    "revision": "d308c92c65efc056b5281ca75de4e3b2",
+    "url": "/static/media/bulb.d308c92c.svg"
+  },
+  {
+    "revision": "cc86f7c07535a3946bc60a755d3ba4e9",
+    "url": "/static/media/bulb_1.cc86f7c0.svg"
+  },
+  {
+    "revision": "8c342fb6097eb5dae511afa740bd0165",
+    "url": "/static/media/bulb_2.8c342fb6.svg"
+  },
+  {
+    "revision": "df45c1213c120cc0b5f42e11c8d31150",
+    "url": "/static/media/bulb_3.df45c121.svg"
+  },
+  {
+    "revision": "38aee7080ebe2004832ce425ba0312cd",
+    "url": "/static/media/electric.38aee708.svg"
+  },
+  {
+    "revision": "88018f9619b0a2167ac49dcb217db7a0",
+    "url": "/static/media/mainlogo.88018f96.svg"
+  },
+  {
+    "revision": "3ab4366dba2d6f62a97cafe529584e7d",
+    "url": "/static/media/megaphone.3ab4366d.svg"
+  }
+]);
