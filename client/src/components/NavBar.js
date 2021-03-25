fd06d2f5212287aaeffba077c9626096318cca53
Felix Nagel <felixnagel105@gmail.com>
2021-03-25 13:03:39 +0100

    added auth0 authentication for the dashboard

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,28 @@
-import { slide as Menu } from 'react-burger-menu';
-import React from 'react'; 
-import '../styles/components/nav.scss';
+import { slide as Menu } from 'react-burger-menu'
+import React from 'react'
+import '../styles/components/nav.scss'
+import AuthenticationButton from '../auth/AuthenticationButton'
 
 const NavBar = () => {
-
-    return(
-        <div>
-            <Menu right width={'100%'}>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="privacy" className="menu-item" href="/privacy">Privacy Policy</a>
-                <a id="sources" className="menu-item" href="/sources">Sources</a>
-            </Menu>
-        </div>
-    )
+	return (
+		<div>
+			<Menu right width={'100%'}>
+				<a id='home' className='menu-item' href='/'>
+					Home
+				</a>
+				<a id='about' className='menu-item' href='/about'>
+					About
+				</a>
+				<a id='privacy' className='menu-item' href='/privacy'>
+					Privacy Policy
+				</a>
+				<a id='sources' className='menu-item' href='/sources'>
+					Sources
+				</a>
+				<AuthenticationButton />
+			</Menu>
+		</div>
+	)
 }
 
-export default NavBar;
+export default NavBar
